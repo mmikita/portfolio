652e61924285c20901003042baab4078e9daf4e4
paster0 <paster01@gmail.com>
2020-06-18 20:13:23 +0200

    no message

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,49 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "./navbar"
 import Baner from "./baner"
-
+import About from "./about"
 import "./layout.css"
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+export const query = graphql`
+{
+  allStrapiProjects(filter: {featured: {eq: true}}) {
+    nodes {
+      github
+      id
+      description
+      title
+      url
+      image {
+        childImageSharp {
+          fluid {
+            src
+          }
         }
       }
+      stack {
+        id
+        title
+      }
     }
-  `)
+  }
+}
+`
 
+
+const Layout = ({ children }) => {
+console.log({query})
   return (
-    <>
-      <Navbar />
-      <Baner/>
-    </>
+  <div>
+    <Navbar />
+    <Baner />
+    <div className="container">
+      <About />
+    </div>
+  </div>
+
   )
 }
 
@@ -36,4 +51,9 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
+
+
 export default Layout
+
+
+
