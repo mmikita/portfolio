c1ae20a01e949b3a72dbe47b648eefed952cf347
paster0 <paster01@gmail.com>
2020-06-20 18:25:03 +0200

    no message

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
     return <div className="footer">
-        <p>I'm baby humblebrag XOXO affogato banh mi squid, 8-bit edison bulb williamsburg flexitarian tofu iPhone semiotics salvia jean shorts. Af readymade cold-pressed kitsch, poutine actually copper mug drinking vinegar before they sold out ugh marfa asymmetrical cardigan squid. Live-edge retro coloring book venmo microdosing ennui, williamsburg pinterest subway tile XOXO vaporware. Vinyl butcher unicorn typewriter. Glossier cold-pressed irony pork belly. Cred master cleanse jianbing farm-to-table air plant actually kale chips everyday carry gentrify glossier distillery bespoke letterpress tousled hella. PBR&B actually williamsburg, tousled heirloom woke pickled yr chia.</p>
+        <p>Stopka</p>
 
 
     </div>
