aecd43eee7322abefcd97c39171c361a2f6a4a90
paster0 <paster01@gmail.com>
2020-06-23 19:35:29 +0200

    no message

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,20 +3,21 @@ import { AiFillLinkedin } from "react-icons/fa"
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import { Link,  animateScroll as scroll} from 'react-scroll'
+import { Link, animateScroll as scroll } from 'react-scroll'
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 
 const Footer = () => {
     return <div className="footer">
         <div className="emailContainer">
-            <a href="#">m.mikita893@gmail.com</a>
+            <a href="#"><span className="emailIcon"><MailOutlineIcon /></span><span className="email">m.mikita893@gmail.com</span></a>
         </div>
         <div>        <ul>
             <li class="linkedin"><a href="https://www.linkedin.com/in/micha%C5%82-mikita-433b69142/" title="linkedin" target="_blank"><LinkedInIcon /></a></li>
             <li class="github"><a href="https://github.com/mmikita" title="github" target="_blank"><GitHubIcon /></a></li>
         </ul>
         </div>
-<div className="goToUp"><Link to='top' smooth={true} duration={500} ><ArrowUpwardIcon/></Link></div>
+        <div className="goToUp"><Link to='top' smooth={true} duration={500} ><ArrowUpwardIcon /></Link></div>
 
 
     </div>
