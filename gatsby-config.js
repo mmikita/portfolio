66add7b3a994cc58dad13efd35c24875d1eb7a38
paster0 <paster01@gmail.com>
2020-07-07 19:49:12 +0200

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `http://185.201.112.10:1337`,
         queryLimit: 1000, // Default to 100
         // contentTypes: [`jobs`, `projects`, `blogs`, ],
         //singleTypes:[`about` ]
