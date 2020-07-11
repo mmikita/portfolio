5b6827bddf0885dae1aa8de5d914b02c8bb73555
paster0 <paster01@gmail.com>
2020-07-11 12:09:28 +0200

    no message

--- src/components/about.js
+++ src/components/about.js
@@ -3,8 +3,8 @@ import React from "react"
 const About = () => {
     return <div id="#aboutMe" className="aboutWrapper">
         <h1>About me</h1>
-        <p>I'm Web Developer interested in technologies like Java, HTML, CSS, JavaScript, PHP, MySQL, various frameworks and other technologies. I also know CMS like wordpress, joomla and prestashop.
-I enjoy at challenges at work like learning new technologies in new projects and figure out how to handle in new environment. I love to master new technologies and I think that, the learning new things isn't just the smart thing to do, it's  reallly necessary to not to fall into stagnation and routine. Creating websites in frontend and backend is my work and passion.
+        <p>I'm a web developer interested in technologies like Java, HTML, CSS, JavaScript, PHP, MySQL, various frameworks and other technologies. I also know CMSs like Wordpress, Joomla and Prestashop.
+        I enjoy challenges at work like learning new technologies in new projects and figuring out how to handle a new environment. I love to master new technologies and I think that learning new things isn't just the smart thing to do, it's really necessary to not to fall into stagnation and routine. Creating websites frontend and backend is my work and passion.
 
 
 
